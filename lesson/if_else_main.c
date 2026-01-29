e6f278c37c9447d8d7fbc3c62b922dec360770f4
h-kwb <k.hiroto.6302@gmail.com>
2026-01-29 13:46:55 +0900

    Update if_else_main.c

--- lesson/if_else_main.c
+++ lesson/if_else_main.c
@@ -9,13 +9,17 @@ int main(int argc, char** argv) {
   
   // 入力を受け取る
   fgets(buf, sizeof(buf), stdin);
-  sscanf(buf, "%d", &a);
+  // 入力チェック
+  if (sscanf(buf, "%d", &a) != 1) {
+    printf("数値を入力してください。\n");
+    return 1; // error終了
+  }
   
   // 値が正負か判定
   if (a > 0) {
     printf("%dは、正の数です。\n", a);
   } else if (a < 0){
-    printf("%dは、負の数です。"\n, a);
+    printf("%dは、負の数です。\n", a);
   } else {
     printf("%dは、0です。\n", a);
   }
