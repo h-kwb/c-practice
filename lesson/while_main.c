e70bc18143b0b71b97b4945b7716e28f223dba58
h-kwb <k.hiroto.6302@gmail.com>
2026-01-30 09:26:53 +0900

    Create while_main.c

---
+++ lesson/while_main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+  // 変数宣言
+  int i = 0;
+
+  // whileループ
+  while (i <= 6) {
+    printf("%d ", i);
+    i++;
+  }
+  printf("\n");
+  return 0;
+}
